a02752b5b0480bf59e5d484bf6633093f86a4fb5
CodyBense <codybense@gmail.com>
2026-05-12 17:34:28 -0400

    started search func

--- cmd/search.go
+++ cmd/search.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -8,20 +7,23 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"os/exec"
 )
 
 // searchCmd represents the search command
 var searchCmd = &cobra.Command{
 	Use:   "search",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Search for a player's stats",
+	Long:  `Search for either all the stats of a player or individual stats.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("search called")
+		pyCmd := exec.Command("python3", "-c", "from", "modules", "import", "data;", fmt.Sprintf("data.select_player(%s)", args[0]))
+		out, err := pyCmd.CombinedOutput()
+		if err != nil {
+			fmt.Println("Error executing Python code:", err)
+			return
+		}
+		fmt.Println(string(out))
 	},
 }
 
