4b9898a96fee73e6713cc130122e7e59a8599de6
CodyBense <codybense@gmail.com>
2026-05-13 16:43:02 -0400

    Updated to print error if player tag is not given and reduced redundent code

--- cmd/search.go
+++ cmd/search.go
@@ -16,26 +16,23 @@ var searchCmd = &cobra.Command{
 	Short: "Search for a player's stats",
 	Long:  `Search for either all the stats of a player or individual stats.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 1 {
+			fmt.Println("Error: Player tag is required")
+			return
+		}
+
 		statFlag, err := cmd.Flags().GetString("stat")
 		if err != nil {
 			return
 		}
 
+		var pyScript string
 		if statFlag != "" {
-			pyScript := fmt.Sprintf("from modules import data; data.print_stat(data.get_stat('%s','%s'))", args[0], statFlag)
-			pyCmd := exec.Command("python3", "-c", pyScript)
-			pyCmd.Dir = "/home/cody/workspaces/github/CodyBense/cdl-stats"
-
-			out, err := pyCmd.CombinedOutput()
-			if err != nil {
-				fmt.Println("Error executing Python code:", err)
-				return
-			}
-			fmt.Println(string(out))
-			return
+			pyScript = fmt.Sprintf("from modules import data; data.print_stat(data.get_stat('%s','%s'))", args[0], statFlag)
+		} else {
+			pyScript = fmt.Sprintf("from modules import data; data.print_stats(data.select_player('%s'))", args[0])
 		}
 
-		pyScript := fmt.Sprintf("from modules import data; data.print_stats(data.select_player('%s'))", args[0])
 		pyCmd := exec.Command("python3", "-c", pyScript)
 		pyCmd.Dir = "/home/cody/workspaces/github/CodyBense/cdl-stats"
 
