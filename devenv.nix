902af6f42bb2f0b908ffe6a3d9990322fcf54a34
CodyBense <codybense@gmail.com>
2026-07-24 11:37:50 -0400

    updated devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -5,48 +5,33 @@
   inputs,
   ...
 }:
-
 {
-  # https://devenv.sh/basics/
-  # env.GREET = "devenv";
+  # languages.rust.enable = true;
 
-  # https://devenv.sh/packages/
-  packages = with pkgs; [
-  ];
+  # packages = with pkgs; [
+  # ];
 
-  # https://devenv.sh/languages/
-  languages.rust.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
-  # scripts.hello.exec = ''
-  #   echo hello from $GREET
-  # '';
-
-  # https://devenv.sh/basics/
-  enterShell = ''
-    echo "Hello from rust devenv!"
-  '';
-
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # services.postgres = {
+  #   enable = true;
+  # createDatabase = true;
+  # initialDatabases = [
+  #   {
+  #     name = "cdl_stats";
+  #     initialSQL = ''
+  #       CREATE TABLE players (
+  #         tag VARCHAR PRIMARY KEY,
+  #         kd REAL NOT NULL,
+  #         hp_kd REAL NOT NULL,
+  #         hp_k_10m REAL NOT NULL,
+  #         snd_kd REAL NOT NULL,
+  #         ovl_kd REAL NOT NULL,
+  #         ovl_k_10m REAL NOT NULL,
+  #         created_at TIMESTAMPTZ DEFAULT NOW()
+  #       );
+  #     '';
+  #   }
+  # ];
+  # listen_addresses = "127.0.0.1";
+  # port = 5432;
   # };
-
-  # https://devenv.sh/tests/
-  # enterTest = ''
-  #   echo "Running tests"
-  #   git --version | grep --color=auto "${pkgs.git.version}"
-  # '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
 }
